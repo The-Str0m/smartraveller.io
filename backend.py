b4360587ade54ebf2380257e242c05f3e3baee6c
The-Str0m <pranavsagiraju@gmail.com>
2025-11-22 13:07:08 +0530

    Update backend.py
    added all bus stops in tampere

--- backend.py
+++ backend.py
@@ -2,6 +2,7 @@ import requests
 import time
 import threading
 from flask import Flask, jsonify
+from pyproj import Transformer
 
 app = Flask(__name__)
 
@@ -12,10 +13,10 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
 
-url = "http://data.itsfactory.fi/journeys/api/1/gtfs-rt/vehicle-positions/json"
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36',
+BUS_URL = "http://data.itsfactory.fi/journeys/api/1/gtfs-rt/vehicle-positions/json"
+bus_headers = {
+    'User-Agent': 'Mozilla/5.0',
     'Accept': 'application/json'
 }
 
@@ -23,34 +24,87 @@ bus_data = []
 
 @app.route('/buses')
 def buses():
-    global bus_data
     return jsonify(bus_data)
+
 
 def update_vehicle_data():
     global bus_data
     while True:
         try:
-            data = requests.get(url, headers=headers).json()
+            data = requests.get(BUS_URL, headers=bus_headers).json()
             vehicles = []
-                
+
             for vehicle in data.get("entity", []):
-                vehicle_position = vehicle["vehicle"]["position"]
+                pos = vehicle["vehicle"]["position"]
                 vehicle_id = vehicle["vehicle"]["vehicle"]["id"]
-                lat = vehicle_position['latitude']
-                lng = vehicle_position['longitude']
-                    
+
                 vehicles.append({
                     "id": vehicle_id,
-                    "lat": lat,
-                    "lng": lng
-                })        
+                    "lat": pos["latitude"],
+                    "lng": pos["longitude"]
+                })
+
             bus_data = vehicles
+
         except Exception as e:
-            print(f"error {e}")
+            print("Bus fetch error:", e)
+
         time.sleep(2)
+
+
+
+STOP_URL = "https://geodata.tampere.fi/geoserver/joukkoliikenne/wfs"
+STOP_PARAMS = {
+    "service": "WFS",
+    "version": "2.0.0",
+    "request": "GetFeature",
+    "typeNames": "joukkoliikenne:joli_bussi_ja_ratikkapysakit_gsview",
+    "outputFormat": "application/json"
+}
+
+#converting finnish coordinates
+transformer = Transformer.from_crs("EPSG:3067", "EPSG:4326", always_xy=True)
+
+stops_data = []
+
+
+def load_stops():
+    global stops_data
+    try:
+        resp = requests.get(STOP_URL, params=STOP_PARAMS)
+        data = resp.json()
+
+        stops = []
+
+        for f in data["features"]:
+            x, y = f["geometry"]["coordinates"]
+            lon, lat = transformer.transform(x, y)
+
+            stops.append({
+                "id": f["properties"]["tunniste"],
+                "name": f["properties"]["nimi"],
+                "lat": lat,
+                "lng": lon,
+                "municipality": f["properties"]["kunta"],
+                "zone": f["properties"]["tariffivyohyke"],
+            })
+
+        stops_data = stops
+
+    except Exception as e:
+        print("stop download error - :", e)
+
+
+@app.route('/stops')
+def stops():
+    return jsonify(stops_data)
+
+
+load_stops()
 
 update_thread = threading.Thread(target=update_vehicle_data, daemon=True)
 update_thread.start()
+
 
 if __name__ == '__main__':
     PORT = 8080
